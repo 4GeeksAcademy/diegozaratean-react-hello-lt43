ead697ca6a49008dcdb732884d7d357a7dd76b5c
diego <diegozaratean@gmail.com>
2025-01-10 22:57:11 +0000

    first react class

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -1,10 +1,22 @@
 import React from "react";
 
+
+//4. Importar compoonente
+import Nav from "./nav";
+
+//5. Usar componente
+
+
 const Home = () => {
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hola bienvenidos a React :)</h1>
-		</div>
+		<>
+			<h1>Header</h1>
+			<Nav />
+			<div className="text-center">
+				<h1 className="text-center mt-5">Hola bienvenidos a React :)123</h1>
+			</div>
+			<h1>Footer</h1>
+		</>
 	);
 };
 
---
+++ src/js/components/Service.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import Nav from "./nav";
+
+const Service = () => {
+    return (
+        <>
+            <h1>Header</h1>
+            <Nav />
+            <div className="text-center">
+                <h1 className="text-center mt-5">Service</h1>
+            </div>
+            <h1>Footer</h1>
+        </>
+    );
+};
+
+export default Service;
